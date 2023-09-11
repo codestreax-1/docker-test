7a46b019b16654b4a60d4b3b7f8f063f20e6c055
codestreax-1 <144151248+codestreax-1@users.noreply.github.com>
2023-09-12 00:58:50 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,7 +1,69 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<vector>
+using namespace std;
 
-        #include <iostream>
-        using namespace std;
-        int main() {
-            cout<<"checking execution time";
-            return 0;
+class Solution{
+    public:
+    int maxElementInArray(int n, vector<int> &arr){
+        if(n == 0) return 0;
+        int max = arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
         }
+        return max;
+    }
+};
+
+int main(){
+    ifstream file("test-cases.txt");
+    string line;
+
+    getline(file,line);
+    int ntc = stoi(line);
+
+    int *sizes = new int[ntc];
+    vector<vector<int>> arrays(ntc);
+    int *ans = new int[ntc];
+
+    for(int i=0;i<ntc;i++){
+        getline(file, line);
+        stringstream ss(line);
+        
+        string temp;
+        getline(ss,temp,',');
+        sizes[i] = stoi(temp);
+        getline(ss,temp,',');
+
+        stringstream ss2(temp);
+        while(ss2>>temp){
+            arrays[i].push_back(stoi(temp));
+        }
+        getline(ss,temp,',');
+        ans[i] = stoi(temp);
+    }
+
+        Solution sol;
+
+        bool check = true;
+        int count = 0;
+        for(int i=0;i<ntc;i++){
+            cout<<i<<" ";
+            if(sol.maxElementInArray(sizes[i],arrays[i])!=ans[i]){
+                check = false;
+                count++;
+            }
+        }
+        if(check){
+            cout<<"all test cases passed"<<endl;
+        }
+        else{
+            cout<<"some test cases failed"<<endl;
+        }
+
+    return 0;
+}
